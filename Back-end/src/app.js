4244d73e9f4ccebdbbf60de33b16efa3857438ce
Doniben <doniben@esperanto.co>
2020-12-28 21:20:45 -0500

    [FEATURE] api and crud working

--- Back-end/src/app.js
+++ Back-end/src/app.js
@@ -4,7 +4,9 @@ const morgan = require('morgan');
 const app = express()
 
 app.set('port', process.emit.PORT || 4000);
-app.use(morgan('dev'))
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/courses", require('./routes/courses.routes'))
 
--- Back-end/src/controllers/courses.controller.js
+++ Back-end/src/controllers/courses.controller.js
@@ -1,19 +1,27 @@
-const coursesCtrl = {}
+const coursesCtrl = {};
+const Course = require('../models/course');
 
-coursesCtrl.getCourses = (req, res) => {
-    res.send('get courses')
-}
-coursesCtrl.createCourse = (req,res) => {
-    res.send('create courses')
-}
-coursesCtrl.getCourse = (req, res) => {
-    res.send('get just one course')
-}
-coursesCtrl.updateCourse = (req, res) => {
-    res.send('update a course')
-}
-coursesCtrl.deleteCourse = (req, res) => {
-    res.send('delete a course')
+coursesCtrl.getCourses = async (req, res) => {
+    const courses = await Course.find();
+    res.json(courses);
+};
+coursesCtrl.createCourse = async (req,res) => {
+    const newCourse =  new Course(req.body)
+    await newCourse.save();
+    console.log(newCourse);
+    res.send({message: 'Nuevo curso creado'});
+};
+coursesCtrl.getCourse = async (req, res) => {
+    const course = await Course.findById(req.params.id);
+    res.send(course);
+};
+coursesCtrl.updateCourse = async (req, res) => {
+    await Course.findByIdAndUpdate(req.params.id, req.body)
+    res.json('Curso Actualizado')
+};
+coursesCtrl.deleteCourse = async (req, res, next) => {
+    await Course.findByIdAndRemove(req.params.id);
+    res.json({ status: "Curso Eliminado" });
 }
 
-module.exports = coursesCtrl
+module.exports = coursesCtrl;
