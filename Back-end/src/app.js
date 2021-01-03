29d73933ccb8c3154cf6cad9ef32302f925a7568
Doniben <doniben@esperanto.co>
2021-01-02 20:44:41 -0500

    [FEATURE] users control

--- Back-end/src/app.js
+++ Back-end/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors({origin: "http://localhost:4200"}));
 app.set('port', process.emit.PORT || 4000);
 app.use(morgan('dev'));
 app.use(express.json());
